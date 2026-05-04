edcaac72a45d8cccedffad09836a0915eb603c37
Achinta <achintasiddhanta0304@gmail.com>
2026-05-04 15:53:37 +0530

    implement packages/db

---
+++ packages/db/src/index.ts
@@ -0,0 +1,2 @@
+export { prisma } from "./lib/prisma.js"; // exports instance of prisma
+export * from "../generated/prisma/client.js"; // exports generated types from prisma
--- packages/db/src/lib/prisma.ts
+++ packages/db/src/lib/prisma.ts
@@ -1,11 +1,16 @@
-import "dotenv/config";
+import { PrismaClient } from "../../generated/prisma/client.js"
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "../generated/prisma/client.js";
 
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+});
 
-const connectionString = `${process.env.DATABASE_URL}`;
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
-const adapter = new PrismaPg({ connectionString });
-const prisma = new PrismaClient({ adapter });
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    adapter,
+  });
 
-export { prisma };
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
