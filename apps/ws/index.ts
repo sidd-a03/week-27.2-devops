800d35407452e58d7912a3b58ce46bdc09c30c79
Achinta <achintasiddhanta0304@gmail.com>
2026-05-04 16:51:57 +0530

    implement ws

---
+++ apps/ws/index.ts
@@ -0,0 +1,25 @@
+import { prisma } from "@repo/db";
+
+
+Bun.serve({
+    port: 8081,
+    fetch(req, server) {
+      // upgrade the request to a WebSocket
+      if (server.upgrade(req)) {
+        return; // do not return a Response
+      }
+      return new Response("Upgrade failed", { status: 500 });
+    },
+    websocket: {
+        message(ws, message) {
+            prisma.user.create({
+                data: {
+                    username: Math.random().toString(),
+                    email: Math.random().toString(),
+                    password: Math.random().toString()
+                }
+            })
+            ws.send(message);
+        },
+    },
+});
